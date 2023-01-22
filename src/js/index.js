3bd482a1885e0c0a93651269c9d41c1815418a1b
Bruna Beatriz <bruna.blecha@gmail.com>
2023-01-22 11:15:10 -0300

    Inserção Js

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+/*  
+
+OBJETIVO 1 - Quando o usuário clicar no botão 'Veja o trailer', devemos abrir a modal com o video do trailer
+    - passo 1 - Atrelar o elemento que representa o botão com o js
+    - passo 2 - Identificar quando o usuário clicar no botão
+    - passo 3 - Pegar o elemento da modal no js
+    - passo 4 - Abrir a modal na tela
+
+OBJETIVO 2 - Quando o usuário clicar no botão 'X', devemos fechar a modal 
+    - passo 1 - Pegar o elemento fechar modal usando jd
+    - passo 2 - Identificar quando o usuário clicar no X
+    - passo 3 - Fechar a modal
+*/
+
+console.log('Mostrar o document'document);
+
+console.log(document.querySelector(".botao-trailer"));
