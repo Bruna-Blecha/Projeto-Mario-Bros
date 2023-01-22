0f5a9348f657eedce3868ac5560b297508b9de95
Bruna Beatriz <bruna.blecha@gmail.com>
2023-01-22 16:22:23 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
 const modal = document.querySelector(".modal");
 const video = document.getElementById("video");
 const linkDoVideo = video.src;
@@ -7,8 +8,6 @@ botaoTrailer.addEventListener("click", () => {
     modal.classList.add("aberto");
     video.setAttribute("src", linkDoVideo)
 });
-
-const botaoFecharModal = document.querySelector(".fechar-modal");
 
 botaoFecharModal.addEventListener("click", () => {
     modal.classList.remove("aberto");
